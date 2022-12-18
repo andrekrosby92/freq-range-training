0b27ef8ba888b7d9e9f84fff5bb341ae1b20c620
André Krosby <andre.krosby92@icloud.com>
2022-12-18 16:52:37 +0100

    fix: missing bg-color

--- pages/index.tsx
+++ pages/index.tsx
@@ -117,7 +117,7 @@ const Home: NextPage = () => {
               </div>
 
               <button
-                className="text-white bg-blue-[rgb(0,122,255)] hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-3 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                className="text-white bg-[rgb(0,122,255)] hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-3 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                 onClick={handleOnNextChallengeClick}
                 type="button"
               >
